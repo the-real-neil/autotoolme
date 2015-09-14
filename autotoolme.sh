f7859b8a17fc65624d362001357a924e44e9469f
rubicks <rubicks@users.noreply.github.com>
2015-09-14 16:01:42 -0400

    edits autotoolme.sh, changes curl to wget

--- autotoolme.sh
+++ autotoolme.sh
@@ -5,19 +5,19 @@ set -e
 _here=$(pwd)
 
 cd ${_here}
-curl -sL https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz | tar -xzf -
+wget -O- https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz | tar -xzf -
 cd autoconf-2.69
 ./configure
 make install
 
 cd ${_here}
-curl -sL https://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz | tar -xzf -
+wget -O- https://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz | tar -xzf -
 cd automake-1.15
 ./configure
 make install
 
 cd ${_here}
-curl -sL https://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.gz | tar -xzf -
+wget -O- https://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.gz | tar -xzf -
 cd libtool-2.4.6
 ./configure
 make install
